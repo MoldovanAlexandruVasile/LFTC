6bbaa19631325990cee03c9b5b89f94bf59bd1ea
Moldovan Alenxadru <moldovan_alexandru97@yahoo.com>
2018-11-04 21:47:16 +0200

    Finished lab2

--- LFTC/src/main/java/Scanner.java
+++ LFTC/src/main/java/Scanner.java
@@ -1,29 +1,28 @@
 import javafx.util.Pair;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Scanner {
+    private Integer lineNumber = 1;
     private Integer lastIdentifierPos = 0;
     private Integer lastConstantPos = 0;
     private File file;
-    private File tableFile;
+    private File PIFFile;
     private File identifiersTableFile;
     private File constantsTableFile;
     private List<HashMap<String, Pair<Integer, Integer>>> PIF = new ArrayList<>();
-    private List<HashMap<String, Integer>> identifiersTable = new ArrayList<>();
-    private List<HashMap<String, Integer>> constantsTable = new ArrayList<>();
+    private HashMap<String, Integer> identifiersTable = new HashMap<>();
+    private HashMap<String, Integer> constantsTable = new HashMap<>();
     private HashMap<String, Integer> tableTxt = new HashMap<>();
 
     Scanner(String fileName) {
         try {
             file = new File(fileName);
-            tableFile = new File("Table.txt");
+            PIFFile = new File("PIF.txt");
             identifiersTableFile = new File("IdentifiersTable.txt");
             constantsTableFile = new File("ConstantsTable.txt");
-            tableFile.createNewFile();
+            PIFFile.createNewFile();
             identifiersTableFile.createNewFile();
             constantsTableFile.createNewFile();
         } catch (Exception e) {
@@ -34,7 +33,7 @@ public class Scanner {
 
     private void populateTable() {
         try {
-            BufferedReader br = new BufferedReader(new FileReader(new File("InitialTable.txt")));
+            BufferedReader br = new BufferedReader(new FileReader(new File("IdTable.txt")));
             String line = br.readLine();
             while (line != null) {
                 String[] pair = line.split(" ");
@@ -51,30 +50,26 @@ public class Scanner {
             BufferedReader br = new BufferedReader(new FileReader(file));
             String line = br.readLine();
             while (line != null) {
+                if (line.charAt(line.length() - 1) != ';' &&
+                        !(line.contains("{") || line.contains("}") || line.contains("(") || line.contains(")"))) {
+                    System.out.println("Syntax error at line number " + lineNumber);
+                    break;
+                }
                 String[] lineSplit = line.split(" ");
                 for (Integer pos = 0; pos < lineSplit.length; pos++) {
-                    String element = lineSplit[pos].replaceAll("'|,|\"|;|[ ]+|[(]|[)]|[{]|[}]|\t|\n|\r", "");
-                    Integer nature = checkElementNature(element);
-                    HashMap<String, Pair<Integer, Integer>> firstMap = new HashMap<>();
-                    HashMap<String, Integer> map = new HashMap<>();
-                    if (nature == -1) {
-                        Integer elementPos = tableTxt.get(element);
-                        firstMap.put(element, new Pair<>(elementPos, nature));
-                        PIF.add(firstMap);
-                    } else if (nature == 1) {
-                        map.put(element, lastConstantPos);
-                        constantsTable.add(map);
-                        firstMap.put(element, new Pair<>(lastConstantPos, nature));
-                        PIF.add(firstMap);
-                        lastConstantPos++;
-                    } else if (nature == 0 && element.length() <= 8) {
-                        map.put(element, lastIdentifierPos);
-                        identifiersTable.add(map);
-                        firstMap.put(element, new Pair<>(lastIdentifierPos, nature));
-                        PIF.add(firstMap);
-                        lastIdentifierPos++;
-                    }
+                    String elem = lineSplit[pos].replaceAll("[ ]+|\t|\n|\r", "");
+                    if (elem.length() != 1 && !elem.equals("")) {
+                        List<String> elements = getElementsToBeAdded(elem);
+                        if (elements.size() == 0)
+                            System.out.println("Syntax error at line number " + lineNumber);
+                        else
+                            for (Integer pos2 = 0; pos2 < elements.size(); pos2++) {
+                                String element = elements.get(pos2);
+                                addElementIntoTable(element);
+                            }
+                    } else addElementIntoTable(elem);
                 }
+                lineNumber++;
                 line = br.readLine();
             }
             writeTables();
@@ -83,6 +78,81 @@ public class Scanner {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    private void addElementIntoTable(String element) {
+        if (!element.equals("")) {
+            if (element.equals("\\("))
+                element = String.valueOf('(');
+            Integer nature = checkElementNature(element);
+            Integer elementP;
+            HashMap<String, Pair<Integer, Integer>> firstMap = new HashMap<>();
+            if (nature == -1) {
+                Integer elementPos = tableTxt.get(element);
+                firstMap.put(element, new Pair<>(elementPos, nature));
+                PIF.add(firstMap);
+            } else if (nature == 1) {
+                if (!constantsTable.containsKey(element)) {
+                    constantsTable.put(element, lastConstantPos);
+                    lastConstantPos++;
+                }
+                elementP = getElementPos(constantsTable, element);
+                firstMap.put(element, new Pair<>(elementP, nature));
+                PIF.add(firstMap);
+            } else if (nature == 0 && element.length() <= 8) {
+                if (!identifiersTable.containsKey(element)) {
+                    identifiersTable.put(element, lastIdentifierPos);
+                    lastIdentifierPos++;
+                }
+                elementP = getElementPos(identifiersTable, element);
+                firstMap.put(element, new Pair<>(elementP, nature));
+                PIF.add(firstMap);
+            } else if (!element.equals(""))
+                System.out.println("Syntax error at line number " + lineNumber);
+        }
+    }
+
+    private List<String> getElementsToBeAdded(String element) {
+        String extraChar = checkExtraCharsExistence(element);
+        List<String> returnList = new ArrayList<>();
+        if (!extraChar.equals("") && String.valueOf(element.charAt(0)).equals(extraChar)) {
+            returnList.add(extraChar);
+            element = element.replaceAll(extraChar, "");
+            if (extraChar.equals(";") || extraChar.equals("\\}") || extraChar.equals("\\{"))
+                return new ArrayList<>();
+            if (tableTxt.containsKey(element))
+                returnList.add(element);
+            else return new ArrayList<>();
+        } else if (!extraChar.equals("") && String.valueOf(element.length() - 1).equals(extraChar)) {
+            element = element.replaceAll(extraChar, "");
+            if (tableTxt.containsKey(element)) {
+                returnList.add(element);
+                returnList.add(extraChar);
+            } else return new ArrayList<>();
+        } else if (!extraChar.equals("")) {
+            String[] lists = element.split(extraChar);
+            Integer extraToBeAdded = lists.length - 1;
+            for (Integer elem = 0; elem < lists.length; elem++) {
+                returnList.add(lists[elem]);
+                if (extraToBeAdded != 0)
+                    returnList.add(extraChar);
+            }
+        } else if (extraChar.equals("")) {
+            returnList.add(element);
+        }
+        return returnList;
+    }
+
+    private Integer getElementPos(HashMap<String, Integer> table, String element) {
+        Integer pos = 0;
+        Iterator it = table.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            if (pair.getKey().equals(element))
+                return pos;
+            pos++;
+        }
+        return -1;
     }
 
     private Integer checkElementNature(String element) {
@@ -98,34 +168,77 @@ public class Scanner {
     private void writeTables() {
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(identifiersTableFile, false));
-            for (Integer i = 0; i < identifiersTable.size(); i++) {
-                HashMap<String, Integer> map = identifiersTable.get(i);
-                String key = map.entrySet().iterator().next().getKey();
-                Integer value = map.entrySet().iterator().next().getValue();
-                writer.write(key + " " + value + "\n");
+            Iterator it = identifiersTable.entrySet().iterator();
+            while (it.hasNext()) {
+                Map.Entry pair = (Map.Entry) it.next();
+                writer.write("'" + pair.getKey() + "'" + " " + pair.getValue() + "\n");
             }
 
             BufferedWriter writer2 = new BufferedWriter(new FileWriter(constantsTableFile, false));
-            for (Integer i = 0; i < constantsTable.size(); i++) {
-                HashMap<String, Integer> map = constantsTable.get(i);
-                String key = map.entrySet().iterator().next().getKey();
-                Integer value = map.entrySet().iterator().next().getValue();
-                writer2.write(key + " " + value + "\n");
+            it = constantsTable.entrySet().iterator();
+            while (it.hasNext()) {
+                Map.Entry pair = (Map.Entry) it.next();
+                writer2.write("'" + pair.getKey() + "'" + " " + pair.getValue() + "\n");
             }
 
-            BufferedWriter writer1 = new BufferedWriter(new FileWriter(tableFile, false));
+            BufferedWriter writer1 = new BufferedWriter(new FileWriter(PIFFile, false));
             for (Integer i = 0; i < PIF.size(); i++) {
                 HashMap<String, Pair<Integer, Integer>> map = PIF.get(i);
                 String key = map.entrySet().iterator().next().getKey();
                 Pair<Integer, Integer> value = map.entrySet().iterator().next().getValue();
-                writer1.write(key + " " + value.getKey() + " " + value.getValue() + "\n");
+                writer1.write("'" + key + "'" + " " + value.getKey() + " " + value.getValue() + "\n");
             }
-
             writer.close();
             writer1.close();
             writer2.close();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    private String checkExtraCharsExistence(String element) {
+        if (element.contains("{"))
+            return "\\{";
+        else if (element.contains("}"))
+            return "\\}";
+        else if (element.contains("-"))
+            return "-";
+        else if (element.contains("!"))
+            return "!";
+        else if (element.contains("!="))
+            return "!=";
+        else if (element.contains("&&"))
+            return "&&";
+        else if (element.contains("("))
+            return "\\(";
+        else if (element.contains(")"))
+            return "\\)";
+        else if (element.contains("*"))
+            return "*";
+        else if (element.contains(","))
+            return ",";
+        else if (element.contains("/"))
+            return "/";
+        else if (element.contains(";"))
+            return ";";
+        else if (element.contains("||"))
+            return "||";
+        else if (element.contains(String.valueOf('"')))
+            return "\"";
+        else if (element.contains("+"))
+            return "\\+";
+        else if (element.contains("<"))
+            return "<";
+        else if (element.contains("<="))
+            return "<=";
+        else if (element.contains("="))
+            return "=";
+        else if (element.contains("=="))
+            return "==";
+        else if (element.contains(">"))
+            return ">";
+        else if (element.contains(">="))
+            return ">=";
+        else return "";
     }
 }
